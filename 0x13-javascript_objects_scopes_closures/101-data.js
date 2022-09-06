c10d3f6657c96f0304c077e155aa29b764b60005
Ayeni Ayodeji <ayeniayodeji2009@gmail.com>
2022-09-06 13:00:23 +0100

    Create 101-data.js

---
+++ 0x13-javascript_objects_scopes_closures/101-data.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/**
+ * An arbitrary object with keys as occurrences and their
+ * corresponding value as a user id.
+ */
+exports.dict = {
+  89: 1,
+  90: 2,
+  91: 1,
+  92: 3,
+  93: 1,
+  94: 2
+};
+Footer
+© 2022 GitHub, Inc.
